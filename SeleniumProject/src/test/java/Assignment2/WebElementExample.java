f5752e988b05add00e90c3740ef8965854a5add7
reshmabibin <reshmabibin@reshmas-mbp>
2024-06-22 22:28:58 +0400

    Assigment 22 June 2024

--- SeleniumProject/src/test/java/Assignment2/WebElementExample.java
+++ SeleniumProject/src/test/java/Assignment2/WebElementExample.java
@@ -1,9 +1,32 @@
 package Assignment2;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
 public class WebElementExample {
+	static WebDriver driver;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		driver=new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().window().maximize();
+driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+System.out.println(driver.getTitle());
+System.out.println(driver.getCurrentUrl());
+
+WebElement linkelement=driver.findElement(By.linkText("OrangeHRM, Inc"));
+linkelement.click();
+
+
+driver.quit();
+
+		
 
 	}
 
